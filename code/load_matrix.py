1199645d646663e5cf6b4d1de0518edb5213e784
Keanu Kerr <keanukerr@gmail.com>
2019-02-05 20:41:07 +0000

    added more results

---
+++ code/load_matrix.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+U = np.load('/afs/inf.ed.ac.uk/user/s18/s1890615/Documents/NLU/Courseworks/NLU_CW1/code/rnn.U.npy')
+V = np.load('/afs/inf.ed.ac.uk/user/s18/s1890615/Documents/NLU/Courseworks/NLU_CW1/code/rnn.V.npy')
+W = np.load('/afs/inf.ed.ac.uk/user/s18/s1890615/Documents/NLU/Courseworks/NLU_CW1/code/rnn.W.npy')
+
+print(U)
+print("U======================")
+print(V)
+print("V======================")
+print(W)
+print("W======================")
